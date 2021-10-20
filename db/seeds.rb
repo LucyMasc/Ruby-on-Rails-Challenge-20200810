b0fd1528aa7e963ecc6b7dbca4069668cdd8945d
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-20 10:39:11 -0300

    Seed for populating database with products.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+products_list = JSON.parse(File.read('products.json'))
+i = 0
+products_list.each do |product|
+
+  # has_product = product.to_h
+  Product.create!(
+    title: (product['title']).delete("\u0000"),
+    product_type: (product['type']).delete("\u0000"),
+    description: (product['description']).delete("\u0000"),
+    filename: (product['filename']).delete("\u0000"),
+    height: product['height'],
+    width: product['width'],
+    price: product['price'],
+    rating: product['rating']
+  )
+  puts "#{i} - #{ product['title'] } product was created!"
+  i += 1
+end
+
+puts "#{Product.count} products created!"
+# .delete("\u0000") due to rails aborted!
+# ArgumentError: string contains null byte
