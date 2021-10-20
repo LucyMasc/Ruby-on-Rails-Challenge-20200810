bb750391aa072d0cf61e203e86855cf044a8a2d8
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-20 10:29:15 -0300

    Created CRUD products resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: proc { [200,{},['Ruby on Rails Challenge 20200810']] }
+
+  scope module: :api do
+    scope module: :v1 do
+      resources :products
+    end
+  end
 end
