5ba7df94013b9c45a70b7de373edb2cf74b9fab7
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-20 10:57:30 -0300

    Generate products controller and actions on controllers/api/v1/products_controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      def index
+        @products = Product.all
+        render json: { status: 'Success', message: 'Loading all Products', data: @products }, status: :ok
+      end
+
+      def show
+        @product = Product.find(params[:id])
+        render json: { status: 'Success', message: 'Loading a Product', data: @product }, status: :ok
+      end
+
+      def create
+        @product = Product.new(product_params)
+
+        if @product.save!
+          render json: { status: 'Success', message: 'Product successfully created', data: @product }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Not able to create a product.', data: @product }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        @product = Product.find(params[:id])
+
+        if @product.update(product_params)
+          render json: { status: 'Success', message: 'Product successfully updated', data: @product }, status: :ok
+        else
+          render json: { status: 'ERRORs', message: 'Not able to update product', data: @product }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+        render json: { status: 'Success', message: 'Product successfully deleted', data: @product }, status: :unprocessable_entity
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:title, :product_type, :description, :filename, :height, :width, :price, :rating)
+      end
+    end
+  end
+end
